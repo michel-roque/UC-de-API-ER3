dbea113d65762f071304c6087b1b75c9a290e1e9
Michel Roque <michel_game@outlook.com>
2023-03-24 16:49:54 -0300

    commit ER3

---
+++ ER1/src/app/models/noticias.ts
@@ -0,0 +1,7 @@
+export interface Noticias {
+    id?: number;
+    titulo: string;
+    descricao: string;
+    imagem: string;
+}
+
---
+++ ER1/src/app/views/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { NoticiasService } from 'src/app/services/noticias.service';
+import { Noticias } from 'src/app/models/noticias';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private noticiaService: NoticiasService) { }
+
+  listaNoticias = [] as Noticias[]
+
+  ngOnInit(): void {
+    this.carregarNoticias()
+  }
+
+  carregarNoticias() {
+    this.noticiaService.getNoticias().subscribe( (noticiasRecebidas: Noticias[]) => {
+      this.listaNoticias = noticiasRecebidas;
+      console.log(this.listaNoticias);
+    } )
+  }
+
+}
+
+
+  
